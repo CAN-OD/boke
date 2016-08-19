13b4070642ad8bbf192602c0aa558effebbdf4da
wwb <8042030278@163.com>
2016-08-19 17:28:50 +0800

    博客模版

--- nodejs/myapp/app.js
+++ nodejs/myapp/app.js
@@ -52,7 +52,7 @@ var server = app.listen(3001, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://', host, port);
 });
 
 
